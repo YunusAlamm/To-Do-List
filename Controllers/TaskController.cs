d871fe0cda4e7134a36353dcad3f22b6ebfcf344
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-07 22:56:16 +0330

    removing unused libraries

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDo_List.Models;
 using ToDo_List.Data;
-using System.Collections.Generic;
-using System.Linq;
-//using Microsoft.EntityFrameworkCore.Update.Internal;
+
 
 namespace ToDo_List.Controllers
 {
