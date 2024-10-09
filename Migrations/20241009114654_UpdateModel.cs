bd826a27e249fa6f36dd58679bfa3616692db22d
YunusAlamm <alamdari.yoonos@gmail.com>
2024-10-09 15:39:44 +0330

    update database

---
+++ Migrations/20241009114654_UpdateModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo_List.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tasks",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedTo",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedTo",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tasks");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Tasks",
+                newName: "Name");
+        }
+    }
+}
