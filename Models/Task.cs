5d2cffa147de48fbb01bb181c18ebe6f0cdc9b70
YunusAlamm <alamdari.yoonos@gmail.com>
2024-10-09 15:39:27 +0330

    Update Task.cs

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,12 +1,22 @@
 namespace ToDo_List.Models
 {
-    public class TodoItem{
-
-
-        public int id { get; set; }
-        
-        public string   Name { get; set; }
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } 
         public bool IsComplete { get; set; }
+        public string Status { get; set; } //  to track task state
+        public string Priority { get; set; } // Added for task urgency
+        public DateTime DueDate { get; set; } //  deadline
+        public DateTime CreationDate { get; set; } 
+        public DateTime LastModifiedDate { get; set; } // Added last update timestamp
+        public string AssignedTo { get; set; } //  responsible person
+        
+        
+        
+        
     }
+
 
 }
